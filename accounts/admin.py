c5f493bd41da48d992e01523e57e4267b35ff75a
Andrew <kyzwork@yandex.ru>
2024-04-22 09:23:28 +0500

    Override User model where phone is main login field

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
+
+
+class PhoneUserAdmin(UserAdmin):
+    """Define admin model for custom User model with no username field."""
+    fieldsets = (
+        (None, {'fields': ('phone', 'password')}),
+        (_('Personal info'), {'fields': ('email', 'first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('phone', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('phone', 'first_name', 'last_name')
+    ordering = ('phone',)
+
+
+admin.site.register(get_user_model(), PhoneUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,48 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 
 # Create your models here.
+
+
+class PhoneUserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+
+    def _create_user(self, phone, password=None, **extra_fields):
+        """Create and save a User with the given phone and password."""
+        if not phone:
+            raise ValueError('Введите номер телефона!')
+        user = self.model(phone=phone, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, phone, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(phone, password, **extra_fields)
+
+    def create_superuser(self, phone, password=None, **extra_fields):
+        """Create and save a SuperUser with the given phone and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(phone, password, **extra_fields)
+
+
+class MobilePhoneOnlyUser(AbstractUser):
+    username = None
+    phone_regex = RegexValidator(regex=r'^9\d{9}$',
+                                 message="Введите мобильный номер телефона в формате: '9001112233' - 9 цифр подряд, без кода страны. Только Россия!")
+    phone = models.CharField(max_length=10, validators=[phone_regex], unique=True)
+
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = []
+
+    objects = PhoneUserManager()
